6020d9c3a1e867f1aa55f6e2c957b59148e338fe
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2023-07-03 01:09:26 +0300

    minor: update gitignore, add cors

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const userRouter = require('./routes/user.routes');
 const postRouter = require('./routes/post.routes');
 
@@ -7,6 +9,9 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 app.use('/api', userRouter);
 app.use('/api', postRouter);
 
